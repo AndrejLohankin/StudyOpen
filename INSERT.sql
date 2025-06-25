008be9d72e2df13ef1bcdd8a3c225e67487c5774
Andrej Lohankin <andrejlohankin@gmail.com>
2025-06-25 15:51:05 +0300

    Update INSERT.sql

--- INSERT.sql
+++ INSERT.sql
@@ -50,7 +50,7 @@ INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
 VALUES('Thus spoke', 134, 3);
 
 INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
-VALUES('The New Eternity', 205, 2);
+VALUES('The my Eternity', 205, 2);
 
 INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
 VALUES('Afterdusk', 134, 3);
@@ -71,28 +71,31 @@ INSERT INTO Collection (Collection_name, Collection_year_of_release)
 VALUES('Веселое', 2025);
 
 INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
-VALUES(7, 2);
+VALUES(1, 2);
+
+INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
+VALUES(2, 4);
+
+INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
+VALUES(3, 1);
+
+INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
+VALUES(4, 1);
+
+INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
+VALUES(5, 3);
+
+INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
+VALUES(6, 3);
+
+INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
+VALUES(7, 3);
 
 INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
 VALUES(8, 4);
 
 INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
-VALUES(9, 1);
-
-INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
-VALUES(13, 1);
-
-INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
-VALUES(14, 3);
-
-INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
-VALUES(16, 3);
-
-INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
-VALUES(17, 3);
-
-INSERT INTO Collection_Music_tracks (Music_track_ID, Collection_ID)
-VALUES(18, 4);
+VALUES(9, 3);
 
 INSERT INTO musical_performers_albums (musical_performer_ID, album_ID)
 VALUES(1, 1);
@@ -120,3 +123,44 @@ VALUES(2, 4);
 
 INSERT INTO musical_performers_musical_genres (musical_genre_ID, musical_performer_ID)
 VALUES(1, 3);
+
+/* Дополнительные данные для проверки работоспособности запросов */
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('myself', 164, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('by myself', 100, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('bemy self', 100, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('myself by', 100, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('by myself by', 100, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('beemy', 100, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('premyne', 100, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('my own', 100, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('own my', 100, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('my', 100, 1);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('oh my god', 100, 1);
+
+INSERT INTO albums (album_name, album_year_of_release)
+VALUES('example album', 2010);
+
+INSERT INTO Music_tracks (Music_track_name, duration, album_ID)
+VALUES('example track', 100, 5);
