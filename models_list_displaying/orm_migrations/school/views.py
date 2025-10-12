2cab1594105dfc4ed133ade893d79f3ac3705766
Andrej Lohankin <andrejlohankin@gmail.com>
2025-10-12 22:19:52 +0300

    Add orm_migrations

---
+++ models_list_displaying/orm_migrations/school/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+from django.views.generic import ListView
+from .models import Student
+
+class StudentsListView(ListView):
+    model = Student
+    template_name = 'school/students_list.html'
+    context_object_name = 'students'
+
+    def get_queryset(self):
+        return Student.objects.prefetch_related('teachers').all()
